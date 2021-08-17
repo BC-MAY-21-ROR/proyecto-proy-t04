054bc39a08f78a27cdef092fecb9d8e7605dac8e
Alejandro Vazquez <avi200599@gmail.com>
2021-08-17 12:48:49 -0500

    Use gem Devise and creation of the model User
    We use the gem devise for the authentication for the users that we are going to have, also
    we create the model user with his own migration
    
    Co-authored-by: RobertoCarMed <betocarrasco3652@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :students
+  
+  devise_for :students
+  devise_for :teachers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
