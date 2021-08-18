df8a36416018663070b0ea853efbef60f6c5637b
RobertoCarMed <betocarrasco3652@gmail.com>
2021-08-18 12:39:33 -0500

    Resolve change

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'home' , to:'pages#index'
   get 'register' , to:'pages#register'
   root "pages#index"
+  devise_for :students
+  devise_for :teachers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,26 @@ ActiveRecord::Schema.define(version: 2021_08_10_220944) do
   enable_extension "plpgsql"
 
   create_table "students", force: :cascade do |t|
+    
     t.string "name"
     t.string "lastname"
     t.string "email"
     t.string "password_digest"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "lastname"
+    t.string "type"
+    t.string "about"
+    t.string "tags"
+    t.string "summary"
+    t.string "object"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
