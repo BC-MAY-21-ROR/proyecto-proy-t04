bbd2e567519d2b3b84b8df2ebf0a8d9932b3ba1f
Alejandro Vazquez <avi200599@gmail.com>
2021-08-18 18:26:10 -0500

    Create registrations controllers for Students and Teachers
    Were created the registration controller for students and teachers, also
    were override the routes for generated the controllers for each type of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :students
-  devise_for :teachers
+  devise_for :students, controllers: {
+    registrations: 'students/registrations'
+  }
+  devise_for :teachers, controllers: {
+    registrations: 'teachers/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
