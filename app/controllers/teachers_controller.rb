3b293cba209eaf614b88ac0fe2e4f59b33d6e5ef
Alejandro Vazquez <avi200599@gmail.com>
2021-08-25 15:56:24 -0500

    Add links_to in the dropdowns and create method to filter subjects
    We add links_to in the dropdown to redirect to the page where is going to show the teachers, also
    we create a function to filter who are the teachers that have a specific subject
    
    Co-authored-by: RobertoCarMed <betocarrasco3652@gmail.com>

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,19 @@
 class TeachersController < ApplicationController
     
     def index
-        @teachers = Teacher.all
+        @teachers = summary_matches()
     end
+
+    def summary_matches
+        teachers = []
+        
+        Teacher.all.each do |teacher|
+            if teacher.summary.include? params[:summary]
+                teachers.push( teacher )
+            end
+        end
+
+        return teachers
+    end
+
 end
